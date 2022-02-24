73605022d6b9a435c47b3bc8724f2d9c074bb5d1
Kemal-Wuzhi <camelwuzhi@gmail.com>
2022-02-25 00:19:21 +0800

    setting passport and jwt authentication

--- config/passport.js
+++ config/passport.js
@@ -2,11 +2,12 @@ const passport = require('passport')
 const LocalStrategy = require('passport-local')
 const passportJWT = require('passport-jwt')
 const bcrypt = require('bcryptjs')
-const { User } = require('../models')
+const {
+  User
+} = require('../models')
 
 const JWTStrategy = passportJWT.Strategy
 const ExtractJWT = passportJWT.ExtractJwt
-
 
 passport.use(new LocalStrategy({
     usernameField: 'email',
